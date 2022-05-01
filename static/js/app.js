9de40a052fc2e93f0bb6bcd6f725b234df4641a1
Dahee Park <97225263+hi-there-insahae@users.noreply.github.com>
2022-05-02 04:01:21 +0900

    add files

---
+++ static/js/app.js
@@ -0,0 +1,20 @@
+// const checkbox = document.getElementById('checkbox');
+const checkbox = document.querySelector('.checkbox');
+console.log(checkbox);
+
+checkbox.addEventListener('click', toggleClick);
+
+// function click(){
+//     if (document.body.classList.contains('dark')){
+//         document.body.classList.remove('dark')
+//         console.log('convert into Light Mode');
+//     } else {
+//         document.body.classList.add('dark')
+//         console.log('convert into Dark Mode');
+//     }
+// }
+
+function toggleClick() {
+    document.body.classList.toggle('light');
+    console.log('working!')
+}
